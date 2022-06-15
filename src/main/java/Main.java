dce27cb5ee2dcd14576bb45d005958e1ffacf310
giovaner10 <giovanerr10@gmail.com>
2022-06-15 10:47:28 -0300

    Classe main

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        InterfaceRun minhaTred = new InterfaceRun("Tarefa 1", 1000);
+        InterfaceRun minhaTre = new InterfaceRun("Tarefa 2", 100);
+
+
+
+
+    }
+}
